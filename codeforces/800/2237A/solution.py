856e239340f1ad084eb48e79a7f1100113198558
Pranav Shewale <shewalepranav66@gmail.com>
2026-09-14 09:46:28 +0530

    AC: 2237A - Destroying Towers

--- codeforces/800/2237A/solution.py
+++ codeforces/800/2237A/solution.py
@@ -5,7 +5,7 @@ def sumlin(l):
         sum+=l[i]
     return sum
         
- 
+ 
 for i in range(t):
     n = int(input())
     lin = input().split()
@@ -15,3 +15,4 @@ for i in range(t):
             if lin[i]<lin[j]:
                 lin[j] = lin[i]
     print(sumlin(lin))
+    
