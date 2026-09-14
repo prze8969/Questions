b54e4ea8b116b2dd2c3912ce3a5fb873d1a0e5f1
Pranav Shewale <shewalepranav66@gmail.com>
2026-09-14 09:43:04 +0530

    AC: 69A - Young Physicist

---
+++ codeforces/1000/69A/solution.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x_sum = 0;
+    int y_sum = 0;
+    int z_sum = 0;
+    int n;
+    cin >> n;
+ 
+    for (int i = 0; i < n; i++)
+    {
+        int x, y, z;
+        cin >> x >> y >> z;
+        x_sum += x;
+        y_sum += y;
+        z_sum += z;
+    }
+    if (x_sum == 0 && y_sum == 0 && z_sum == 0)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+}
