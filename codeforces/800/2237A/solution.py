af452ceefd468ca116cde43b8ace7245feb48ae8
Pranav Shewale <shewalepranav66@gmail.com>
2026-09-14 09:43:15 +0530

    AC: 2237A - Destroying Towers

---
+++ codeforces/800/2237A/solution.py
@@ -0,0 +1,18 @@
+t = int(input())
+def sumlin(l):
+    sum =0
+    for i in range(len(l)):
+        sum+=l[i]
+    return sum
+        
+ 
+for i in range(t):
+    n = int(input())
+    lin = input().split()
+    lin = [int(x) for x in lin]
+    for i in range(n):
+        for j in range(i+1,n):
+            if lin[i]<lin[j]:
+                lin[j] = lin[i]
+    print(sumlin(lin))
+    
